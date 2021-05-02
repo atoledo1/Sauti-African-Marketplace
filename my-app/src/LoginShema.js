71a22372625721424dabecbd6a83012911b5a894
Adriana Toledo Santiago <atoledosantiago1@gmail.com>
2021-05-02 11:39:35 -0500

    6

--- my-app/src/LoginShema.js
+++ my-app/src/LoginShema.js
@@ -1,14 +1,14 @@
-import * as yup from 'yup'
+import * as yup from "yup";
 
 const loginSchema = yup.object().shape({
-    password: yup
+  password: yup
     .string()
     .min(5, "Password must be at least 5 characters")
     .required("Password is required"),
 
-    username: yup
+  username: yup
     .string()
     .min(2, "Username must be at least 2 characters")
-    .required("Username is required")
-}) 
-export default loginSchema
+    .required("Username is required"),
+});
+export default loginSchema;
--- my-app/src/RegisterShema.js
+++ my-app/src/RegisterShema.js
@@ -1,20 +1,20 @@
-import * as yup from 'yup'
+import * as yup from "yup";
 
 const registerSchema = yup.object().shape({
-    password: yup
+  password: yup
     .string()
     .min(5, "Password must be at least 5 characters")
     .required("Password is required."),
 
-    email:yup
+  email: yup
     .string()
     .email("Must enter valid email")
     .min(2, "Email address must be at least 2 characters")
     .required("Email address is required"),
-    
-    username: yup
+
+  username: yup
     .string()
     .min(2, "Username must be at least 2 characters")
-    .required("Username is required")
-}) 
-export default registerSchema
+    .required("Username is required"),
+});
+export default registerSchema;
