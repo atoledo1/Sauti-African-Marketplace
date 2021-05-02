1eba572eb54ad44a4477d7a7c05d2e9c43995882
Adriana Toledo Santiago <atoledosantiago1@gmail.com>
2021-05-02 11:33:50 -0500

    2

--- my-app/src/components/home.js
+++ my-app/src/components/home.js
@@ -1,76 +1,76 @@
-import React, { useEffect, useState, useContext } from 'react'
-import {axiosWithAuth} from '../utils/axiosWIthAUTH'
-import {useHistory} from 'react-router-dom'
-import {PRODUCTS_PATH, USERS_PATH} from '../utils/URLs'
-import {UserContext} from '../App'
+import React, {useEffect, useState, useContext} from "react";
+import {axiosWithAuth} from "../utils/axiosWIthAUTH";
+import {useHistory} from "react-router-dom";
+import {PRODUCTS_PATH, USERS_PATH} from "../utils/URLs";
+import {UserContext} from "../App";
 
-import Listing from './Listing'
-
+import Listing from "./Listing";
 
 const Home = () => {
-    const {push} = useHistory()
-    const [itemsForSale, setItemsForSale] = useState([])
-    const {user, setUser} = useContext(UserContext)
-    const [userList, setUserList] = useState([])
+  const {push} = useHistory();
+  const [itemsForSale, setItemsForSale] = useState([]);
+  const {user, setUser} = useContext(UserContext);
+  const [userList, setUserList] = useState([]);
 
-    useEffect(() => {
-        // get list of all users - single API call providing data to all children
-        axiosWithAuth()
-        .get(`${USERS_PATH}`)
-        .then(res => {
-            setUserList(res.data.data)
-        })
-        .catch(err => {
-            console.log(err)
-        })
-        //set user context - uses username from localStorage if the page was reloaded
-        let usernameSnapshot = user.username
-        if (usernameSnapshot === '') {
-            usernameSnapshot = localStorage.getItem('username')
-        }
-        axiosWithAuth()
-            .get(`${USERS_PATH}`)
-            .then(res => {
-                const currentUser = res.data.data.filter(u => usernameSnapshot === u.username)
-                const userId = currentUser[0].id
-                setUser({
-                    username: usernameSnapshot,
-                    id: userId
-                })
-                // set list of items for sale
-                // triggers children to mount
-                // children require user context to mount correctly
-                axiosWithAuth()
-                    .get(`${PRODUCTS_PATH}`)
-                    .then(res => {
-                        console.log(res)
-                        setItemsForSale(res.data.data)
-                    })
-                    .catch(err => console.log(err))
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    }, [])
-
-    const addItem = () => {
-        push('/add-item')
+  useEffect(() => {
+    // get list of all users - single API call providing data to all children
+    axiosWithAuth()
+      .get(`${USERS_PATH}`)
+      .then((res) => {
+        setUserList(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    //set user context - uses username from localStorage if the page was reloaded
+    let usernameSnapshot = user.username;
+    if (usernameSnapshot === "") {
+      usernameSnapshot = localStorage.getItem("username");
     }
+    axiosWithAuth()
+      .get(`${USERS_PATH}`)
+      .then((res) => {
+        const currentUser = res.data.data.filter(
+          (u) => usernameSnapshot === u.username
+        );
+        const userId = currentUser[0].id;
+        setUser({
+          username: usernameSnapshot,
+          id: userId,
+        });
+        // set list of items for sale
+        // triggers children to mount
+        // children require user context to mount correctly
+        axiosWithAuth()
+          .get(`${PRODUCTS_PATH}`)
+          .then((res) => {
+            console.log(res);
+            setItemsForSale(res.data.data);
+          })
+          .catch((err) => console.log(err));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    return (
-        
-            <div>
-                <h2>All Products</h2>
-                {itemsForSale.map(item => {
-                    return(
-                        <Listing key={item.id} item={item} userList={userList} />
-                    )
-                })}
-                <div>
-                    <button className='btn' onClick={addItem}>Add Product</button>
-            </div>
-        </div>
-    )
-}
+  const addItem = () => {
+    push("/add-item");
+  };
 
-export default Home
+  return (
+    <div>
+      <h2>All Products</h2>
+      {itemsForSale.map((item) => {
+        return <Listing key={item.id} item={item} userList={userList} />;
+      })}
+      <div>
+        <button className="btn" onClick={addItem}>
+          Add Product
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- my-app/src/components/login.js
+++ my-app/src/components/login.js
@@ -1,73 +1,89 @@
-import React, { useState, useContext } from 'react'
-import { useHistory } from 'react-router-dom'
-import * as yup from 'yup'
-import loginSchema from "../LoginShema"
-import axios from 'axios'
-import {UserContext} from '../App'
-import {BASE_URL, LOGIN_PATH} from '../utils/URLs'
-import '../App.css';
+import React, {useState, useContext} from "react";
+import {useHistory} from "react-router-dom";
+import * as yup from "yup";
+import loginSchema from "../LoginShema";
+import axios from "axios";
+import {UserContext} from "../App";
+import {BASE_URL, LOGIN_PATH} from "../utils/URLs";
+import "../App.css";
 
 const LogForm = {
-    username: '',
-    password: ''
+  username: "",
+  password: "",
+};
+
+function Login(props) {
+  const [form, setForm] = useState(LogForm);
+  const [errors, setErrors] = useState([]);
+  const history = useHistory();
+  const {user, setUser} = useContext(UserContext);
+
+  const handleChange = (e) => {
+    setForm({...form, [e.target.name]: e.target.value});
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    loginSchema
+      .validate(form, {abortEarly: false})
+      .then((res) => {
+        // console.log(res)
+        axios
+          .post(`${BASE_URL}${LOGIN_PATH}`, form)
+          .then((res) => {
+            // console.log(res);
+            localStorage.setItem("token", res.data.token);
+            localStorage.setItem("username", form.username);
+            props.setLoggedIn(true);
+            setUser({
+              ...user,
+              username: form.username,
+            });
+            setForm(LogForm);
+            history.push("/home");
+          })
+          .catch((err) => {
+            console.dir(err);
+            setErrors([...errors, {message: "Invalid Login"}]);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+        setErrors([...err.inner]);
+      });
+  };
+
+  return (
+    <div className="log-container">
+      <form onSubmit={handleSubmit}>
+        {errors.map((error) => {
+          return <p>{error.message}</p>;
+        })}
+        <label>
+          {" "}
+          Username:
+          <input
+            id="logname"
+            name="username"
+            type="textbox"
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          {" "}
+          Password:
+          <input
+            id="logpass"
+            name="password"
+            type="password"
+            onChange={handleChange}
+          />
+        </label>
+        <button className="btn" id="logbtn">
+          Log in
+        </button>
+      </form>
+    </div>
+  );
 }
-
-
-
-function Login (props) {
-    const [form, setForm] = useState(LogForm)
-    const [errors, setErrors] = useState([]); 
-    const history = useHistory()
-    const {user, setUser} = useContext(UserContext)
-    
-    const handleChange = (e) => {
-        setForm({...form, [e.target.name]: e.target.value});
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        loginSchema.validate(form, {abortEarly: false})
-        .then(res => {
-            // console.log(res)
-            axios.post(`${BASE_URL}${LOGIN_PATH}`, form)
-            .then(res => {
-                // console.log(res);
-                localStorage.setItem('token', res.data.token)
-                localStorage.setItem('username', form.username)
-                props.setLoggedIn(true);
-                setUser({
-                    ...user,
-                    username: form.username
-                })
-                setForm(LogForm)
-                history.push('/home')
-            })
-            .catch(err => {
-                console.dir(err)
-                setErrors([...errors, {message:'Invalid Login'}]);
-            })
-        })
-        .catch(err => {
-            console.log(err);
-            setErrors([...err.inner]);
-        })
-    }
-    
-    return(
-        <div className='log-container'> 
-            <form onSubmit={handleSubmit}>
-                {errors.map(error =>{
-                    return <p>{error.message}</p>
-                })}  
-                <label> Username: 
-                    <input id='logname' name='username' type='textbox' onChange={handleChange}/>
-                </label>
-                <label> Password: 
-                    <input id='logpass' name='password' type='password' onChange={handleChange}/>
-                </label>
-                <button className='btn' id='logbtn'>Log in</button>
-            </form>
-        </div>
-    )
-}
-export default Login
+export default Login;
