29bf483c0601e0ed7074152bbeb062a31e73a59d
Adriana Toledo Santiago <atoledosantiago1@gmail.com>
2021-05-02 11:39:05 -0500

    5

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,55 +1,54 @@
-import React, { useState } from 'react';
-import { Route, Switch } from 'react-router-dom'
-import Home from './components/Home'
-import Navigation from './components/Navigation'
-import Login from './components/Login'
-import Register from './components/Register'
-import AddItem from './components/AddItem'
-import ModifyItem from './components/ModifyItem'
-import PrivateRoute from './components/PrivateRoute'
+import React, {useState} from "react";
+import {Route, Switch} from "react-router-dom";
+import Home from "./components/Home";
+import Navigation from "./components/Navigation";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import AddItem from "./components/AddItem";
+import ModifyItem from "./components/ModifyItem";
+import PrivateRoute from "./components/PrivateRoute";
 
-import './App.css';
-import MyListings from './components/MyProfile';
+import "./App.css";
+import MyListings from "./components/MyProfile";
 
-export const UserContext = React.createContext()
+export const UserContext = React.createContext();
 
 function App() {
   const [user, setUser] = useState({
-    username: '',
-    id: ''
-  })
+    username: "",
+    id: "",
+  });
 
-const [ loggedIn, setLoggedIn ] = useState(!!localStorage.getItem('token'));
+  const [loggedIn, setLoggedIn] = useState(!!localStorage.getItem("token"));
 
-return (
+  return (
     <UserContext.Provider value={{user, setUser}}>
       <div className="App">
-        <h1 className='title'>African Marketplace</h1>
-       
-        <Navigation loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>
+        <h1 className="title">African Marketplace</h1>
+
+        <Navigation loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
         <Switch>
-          
-          <Route path='/login'>
-            <Login setLoggedIn={setLoggedIn}/>
+          <Route path="/login">
+            <Login setLoggedIn={setLoggedIn} />
           </Route>
 
-          <Route path='/register'>
+          <Route path="/register">
             <Register />
           </Route>
 
-          <PrivateRoute path='/add-item' >
+          <PrivateRoute path="/add-item">
             <AddItem />
           </PrivateRoute>
 
-          <PrivateRoute path='/modify-item/:itemId' >
+          <PrivateRoute path="/modify-item/:itemId">
             <ModifyItem />
           </PrivateRoute>
 
-          <PrivateRoute path='/home'>
+          <PrivateRoute path="/home">
             <Home />
           </PrivateRoute>
-          <PrivateRoute exact path='/mylistings'>
-            <MyListings/>
+          <PrivateRoute exact path="/mylistings">
+            <MyListings />
           </PrivateRoute>
         </Switch>
       </div>
--- my-app/src/utils/axiosWIthAUTH.js
+++ my-app/src/utils/axiosWIthAUTH.js
@@ -1,13 +1,13 @@
-import axios from 'axios'
-import {BASE_URL} from './URLs'
+import axios from "axios";
+import {BASE_URL} from "./URLs";
 
 export const axiosWithAuth = () => {
-    const token = localStorage.getItem('token')
+  const token = localStorage.getItem("token");
 
-    return axios.create({
-        headers: {
-            Authorization: token
-        },
-        baseURL: BASE_URL
-    })
-}
+  return axios.create({
+    headers: {
+      Authorization: token,
+    },
+    baseURL: BASE_URL,
+  });
+};
